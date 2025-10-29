c405f46060ffa2fdd087b4e4683676cfb6aa5c38
Sakib Faturrahman <sakibfaturrahman92@gmail.com>
2025-10-29 21:03:57 +0700

    update frontend

--- src/app.js
+++ src/app.js
@@ -5,8 +5,7 @@ const path = require("path");
 const dotenv = require("dotenv");
 const session = require("express-session");
 
-const rateLimiter = require("./middlewares/rate.middleware");
-const connectDB = require("./config/database");
+// const connectDB = require("./config/database");
 
 dotenv.config(); // Load .env
 
@@ -17,7 +16,6 @@ app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(rateLimiter);
 
 // --- Session (untuk login admin) ---
 app.use(
@@ -31,11 +29,11 @@ app.use(
 
 // --- Static files (gambar & frontend) ---
 app.use("/uploads", express.static(path.join(__dirname, "..", "uploads")));
-app.use(express.static(path.join(__dirname, "..", "public")));
+app.use(express.static(path.join(__dirname, "public")));
 
 // --- Routes ---
-app.use("/api/admin", require("./routes/admin.routes"));
-app.use("/api/gallery", require("./routes/gallery.routes"));
+// app.use("/api/admin", require("./routes/admin.routes"));
+// app.use("/api/gallery", require("./routes/gallery.routes"));
 app.use("/", require("./routes/frontend.routes"));
 
 // --- Error handler ---
@@ -45,11 +43,10 @@ app.use((err, req, res, next) => {
 });
 
 // --- Koneksi Database & Jalankan Server ---
-connectDB().then(() => {
-  const PORT = process.env.PORT || 3000;
-  app.listen(PORT, () => {
-    console.log(`🚀 Server aktif di http://localhost:${PORT}`);
-  });
+// --- Jalankan Server tanpa koneksi database ---
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`🚀 Server aktif di http://localhost:${PORT}`);
 });
 
 module.exports = app;
